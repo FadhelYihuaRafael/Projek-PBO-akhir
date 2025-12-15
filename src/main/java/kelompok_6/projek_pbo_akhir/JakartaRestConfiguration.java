3e69476b1bb5cb481985d933dd77b65e45d16dcc
Ahmad Riza Muzakki <0110224101@student.nurulfikri.ac.id>
2025-12-15 12:37:25 +0700

    setup awal

---
+++ src/main/java/kelompok_6/projek_pbo_akhir/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package kelompok_6.projek_pbo_akhir;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
